55303fe08b5cf91bcf5fe83f35dd0a55a804e2b1
Владислав Глушко <perfectionist.one.love@otlook.com>
2022-02-12 17:10:15 +0700

    fix path CMakeLists.txt & add TEST(ConstTable, readFile)

--- googletest/src/test.cpp
+++ googletest/src/test.cpp
@@ -1,9 +1,26 @@
 #include <gtest/gtest.h>
 
-TEST(HelloTest, BasicAssertions) {
+#include "const_table.hpp"
+#include "var_table.hpp"
 
+#include <filesystem>
+#include <string>
 
-    EXPECT_STRNE("hello", "world");
+TEST(ConstTable, readFile) {
 
-    EXPECT_EQ(7 * 6, 42);
+    using ::std::filesystem::path;
+    using ::std::string;
+
+    const_table<string> key_table(path("file/keywords.txt"));
+
+    ASSERT_EQ(key_table.contains("int"), true);
+    ASSERT_EQ(key_table.contains("main"), true);
+    ASSERT_EQ(key_table.contains("float"), true);
+    ASSERT_EQ(key_table.contains("return"), true);
+
+    ASSERT_EQ(key_table.contains("typename"), false);
+
+    ASSERT_EQ(key_table.get_num("float"),  0);
+    ASSERT_EQ(key_table.get_num("int"),    1);
+    ASSERT_EQ(key_table.get_num("return"), 3);
 }
